1cd17a0d835f1a6457454f4b213a6f0d7bfa4a89
MartL7 <latiapelitos123@gmail.com>
2024-02-02 16:08:20 -0600

    add HasRouter

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import {App} from './App.jsx'
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import { HashRouter  as Router, Route, Routes } from 'react-router-dom'
 import 'bootstrap/dist/css/bootstrap.min.css' 
 import 'bootstrap-icons/font/bootstrap-icons.css'
 import 'bootstrap/dist/js/bootstrap.bundle.min.js'
@@ -18,7 +18,7 @@ root.render(
   <Router>
 
     <Routes>
-       <Route path="/Recomendaciones-Streaming" element={ <App /> } />
+       <Route path="/" element={ <App /> } />
        <Route path="/Peliculas" element={ <ShowPeliculas /> } />
        <Route path="/Animes" element={ <ShowAnimes /> } />
        <Route path="/Series" element={ <ShowSeries /> } />
